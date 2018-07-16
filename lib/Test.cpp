fbcec2fbdb60e9f5a4bb3ae6434264f27826779a
Tyler Robbins <tyler@familyrobbins.com>
2018-07-15 21:26:24 -0700

    [TESTS] Added test library.

---
+++ lib/Test.cpp
@@ -0,0 +1,22 @@
+/**
+ * @author  Tyler Robbins
+ * @file    Test.cpp
+ * @date    2018-07-13 08:43:08
+ */
+
+#include "Test.h"
+
+Hello::Hello(const std::string& m): m_message(m) { }
+Hello::~Hello(){ }
+
+const std::string& Hello::say() const {
+    return m_message;
+}
+
+void Hello::mparam(int, float) {
+    
+}
+void Hello::fparam(std::function<int(float)>) {
+    
+}
+
---
+++ lib/Test.h
@@ -0,0 +1,24 @@
+#ifndef TEST_H
+#define TEST_H
+
+#include <string>
+#include <functional>
+
+#include "API.h"
+
+class API Hello {
+    public:
+        Hello(const std::string& = "Hello, World!");
+        ~Hello();
+
+        const std::string& say() const;
+
+        void mparam(int, float);
+
+        void fparam(std::function<int(float)>);
+
+    private:
+        std::string m_message;
+};
+
+#endif
