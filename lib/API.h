160749cf239c063abfd0c9308aaca0af0eb0286f
Tyler Robbins <tyler@familyrobbins.com>
2018-07-26 15:01:41 -0700

    [API] Updated to support Linux + OSX.

--- lib/API.h
+++ lib/API.h
@@ -2,9 +2,17 @@
 #define API_H
 
 #ifdef EXPORT
+#ifdef _WIN32
 #define API __declspec(dllexport)
 #else
+#define API
+#endif
+#else
+#ifdef _WIN32
 #define API __declspec(dllimport)
+#else
+#define API
+#endif
 #endif
 
 #endif
