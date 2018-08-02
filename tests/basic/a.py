37b9781bc888f1fe889d48d08a89bfa2d2cde84b
Tyler Robbins <tyler@familyrobbins.com>
2018-08-02 13:48:42 -0700

    [TESTS][BASIC] Added more testing code.

--- tests/basic/a.py
+++ tests/basic/a.py
@@ -7,3 +7,22 @@ print("a.py")
 myHello = lib.Test.Hello("foobar")
 
 print(myHello.say())
+
+def foo(a,b):
+    print("The result of {0} + {1} is {2}".format(a,b,a+b))
+
+    return int(a+b)
+
+myHello.fparam(foo)
+
+try:
+    myHello.throwTest()
+except Exception as e:
+    print("Caught an exception from myHello.throwTest:", e)
+
+lib.Test.Hello.sayHello()
+
+myHello = lib.Test.Hello("General Kenobi!")
+
+print(myHello.say())
+
