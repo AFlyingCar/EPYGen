1bf30d4447ad7790c70cbcd5d3b188056e410831
Tyler Robbins <tyler@familyrobbins.com>
2018-08-02 12:13:37 -0700

    [EPY][GENERATION] Added code to use the raw type specification when creating function headers if it is the header for a virtual function. Modified the parameter string generation for C++ to include transformations to C types. Fixed a missing comma issue when calling the python override from a virtual function. Fixed an issue where a return type of void wouldn't be caught. Added code to raise a python NotImplementedError from a wrapped abstract method. No C wrapper will be generated for abstract methods.

--- lib/CppGenerate.py
+++ lib/CppGenerate.py
@@ -100,7 +100,7 @@ def createCPPCtor(klass_name, params, throws, epy, referenced_throws = [], ctor_
 
     return ctor_str + "    }\n"
 
-def createCPPFunctionHeader(cres_type, full_name, function, is_class, is_virtual = False):
+def createCPPFunctionHeader(cres_type, full_name, function, is_class, is_virtual = False, use_raw_types = False):
     func_header = ""
 
     # Don't generate the declspec call on virtual functions (those defined in a class wrapper)
@@ -125,7 +125,11 @@ def createCPPFunctionHeader(cres_type, full_name, function, is_class, is_virtual
 
     pcount = 0
     for p in function.param_list:
-        func_header += p[0].c_type + ' param' + str(pcount) + ','
+        if use_raw_types:
+            p_type = p[0].raw
+        else:
+            p_type = p[0].c_type
+        func_header += p_type + ' param' + str(pcount) + ','
         pcount += 1
 
     if type(cres_type) != str:
@@ -214,23 +218,28 @@ def createCPPVirtualFuncWrapper(func, orig_name, wrap_name):
     rtype = func.rtype.cpp_type if func.rtype.is_reference else func.rtype.raw
 
     # We pass False for is_class so that no `self` parameter gets generated
-    vfunc_str += ident + createCPPFunctionHeader(rtype, func.name, func, False, True)
+    vfunc_str += ident + createCPPFunctionHeader(rtype, func.name, func, False, True, True)
     vfunc_str += (" const" if func.const else "") + " override {\n"
 
     ident += "    "
 
-    param_str = ", ".join(['param' + str(i) for i in range(len(func.param_list))])
+    param_str = ", ".join([p[0].createCTransformation('param' + str(i)) for i,p in enumerate(func.param_list, 0)])
 
     vfunc_str += ident + "if(m_pyobj != nullptr){\n"
     vfunc_str += ident + "    PyObject* result = PyObject_CallMethodObjArgs(m_pyobj, PyBytes_FromString(\"{0}\"), {1}".format(func.name, param_str)
-    vfunc_str += "NULL);\n"
+    vfunc_str += (',' if param_str and not param_str.endswith(',') else '') + "NULL);\n"
     # Return PyObject as CPP type here
-    if func.rtype != "void":
+    if str(func.rtype).strip() != "void":
         vfunc_str += ident + "    " + func.rtype.createPyObjectTransformation("result", "return ") + ";\n"
         # vfunc_str += ident + "    return Epy::PyObjectToType<{0}>(result);\n".format(func.rtype.raw)
 
     vfunc_str += ident + "} else {\n"
-    vfunc_str += ident + "    {3}{0}::{1}({2});\n".format(orig_name, func.name, param_str, "return " if func.rtype != "void" else "")
+    if not func.abstract:
+        vfunc_str += ident + "    {3}{0}::{1}({2});\n".format(orig_name, func.name, param_str, "return " if str(func.rtype).strip() != "void" else "")
+    else:
+        vfunc_str += ident + "    PyErr_SetString(PyExc_NotImplementedError, \"Cannot call {0} as it is abstract.\");\n".format(orig_name)
+        if str(func.rtype).strip() != "void":
+            vfunc_str += ident + "    return nullptr;\n"
     vfunc_str += ident + "}\n"
 
     ident = "    " * 2
@@ -304,8 +313,9 @@ def createCPPClass(klass, epy, reffed_throws):
 
     functions_found = {f.name: 0 for f in klass.functions}
     for f in klass.functions:
-        full_name = klass.name_fmt + '_' + f.name + str(functions_found[f.name])
-        class_string += createCPPFunction(full_name, f.name, f, epy, reffed_throws, True)
+        if not f.abstract:
+            full_name = klass.name_fmt + '_' + f.name + str(functions_found[f.name])
+            class_string += createCPPFunction(full_name, f.name, f, epy, reffed_throws, True)
 
     class_string += "}\n"
 
