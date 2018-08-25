3cf7a48755e32682f46116c0d93022c5df403922
Tyler Robbins <tyler@familyrobbins.com>
2018-08-25 13:01:03 -0700

    [LITERAL] Added missing method definitions.

--- src/Literal.cpp
+++ src/Literal.cpp
@@ -1,9 +1,30 @@
 
 #include "Literal.h"
 
-EpyGen::PyLiteral::PyLiteral(const std::string& name): Section(name) {
+EpyGen::PyLiteral::PyLiteral(const std::string& name): Section(name),
+                                                       m_literal()
+{
 }
 
-EpyGen::CppLiteral::CppLiteral(const std::string& name): Section(name) {
+const std::string& EpyGen::PyLiteral::getLiteral() const {
+    return m_literal;
+}
+
+void EpyGen::PyLiteral::setLiteral(const std::string& literal) {
+    m_literal = literal;
+}
+
+EpyGen::CppLiteral::CppLiteral(const std::string& name): Section(name),
+                                                         m_literal()
+{
+}
+
+
+const std::string& EpyGen::CppLiteral::getLiteral() const {
+    return m_literal;
+}
+
+void EpyGen::CppLiteral::setLiteral(const std::string& literal) {
+    m_literal = literal;
 }
 
